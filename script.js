fbf9e2c63600e509faf17a0d6c60da332fac3d2e
atheneum27 <alatheneum@gmail.com>
2025-08-09 21:23:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -133,14 +133,6 @@ window.addEventListener('storage', (event) => {
 // Initialize the grid
 createGrid();
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDApxCwdqXURI9AXdNvwRK5LDwqDPMXe_I",
   authDomain: "atheneumsync.firebaseapp.com",
@@ -151,10 +143,3 @@ const firebaseConfig = {
   appId: "1:383620444372:web:fc408b459512629d0e8b41",
   measurementId: "G-Y4MX263QBM"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
